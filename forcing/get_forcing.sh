8cee0f0fa8a5459d5e4cd6195db0ba7bc192214c
MEZY03 <2053926770@qq.com>
2025-12-01 01:38:58 +0800

    Initial commit: VIC data preparation scripts

--- forcing/get_forcing.sh
+++ forcing/get_forcing.sh
@@ -7,5 +7,5 @@ cdo -houravg -select,name=SWGDN    MERRA/M2T1NXRAD.5.12.4_MERRA2_*.tavg1_2d_rad_
 cdo -houravg -select,name=LWGAB    MERRA/M2T1NXRAD.5.12.4_MERRA2_*.tavg1_2d_rad_Nx.*.nc4.nc4 forcing_data/longwave_radiation.nc
 cdo -houravg -select,name=PRECTOT  MERRA/M2T1NXFLX.5.12.4_MERRA2_*.tavg1_2d_flx_Nx.*.nc4.nc4 forcing_data/precipitation.nc
 
-# 如果变量维顺序错误，需要用以下NCO代码调换
+# 如果变量维顺序错误, 需要用以下NCO代码调换
 # ncpdq --rdr=time,lat,lon input_file.nc output_file.nc
